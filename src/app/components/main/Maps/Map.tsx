f0b3e19aa63ef6efe0b863ca11fba251ba55eb9d
Renat <renatovrenat17@mail.ru>
2024-07-10 11:23:23 +0300

    third commit

--- src/app/components/main/Maps/Map.tsx
+++
@@ -1,7 +0,0 @@
-export const Map:React.FC = () => { 
-    return ( 
-        <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387194.06214886444!2d-74.30932024581656!3d40.69701930756295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2z0J3RjNGOLdCZ0L7RgNC6LCDQodCo0JA!5e0!3m2!1sru!2sru!4v1720375838338!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-    )
-}
